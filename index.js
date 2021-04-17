7a4d9331f00d6f253a9f1e44eac8a4a223759b98
Ankur Agarwal <agarwalankur58@gmail.com>
2021-04-17 12:06:37 +0530

    pino logger wrapper

---
+++ index.js
@@ -0,0 +1,17 @@
+const pino = require('pino');
+const logger = require('pino')();
+
+process.on('uncaughtException', pino.final(logger, (err, finalLogger) => {
+  finalLogger.error(err, 'uncaughtException')
+  process.exit(1)
+}));
+
+process.on('unhandledRejection', pino.final(logger, (err, finalLogger) => {
+  finalLogger.error(err, 'unhandledRejection')
+  process.exit(1)
+}));
+
+module.exports = {
+  pino,
+  logger
+};
